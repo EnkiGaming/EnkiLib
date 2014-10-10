d49719a387c60cedb50ddc17d95f3f870dc0d177
HaniiPuppy <hanii.puppy@googlemail.com>
2014-10-10 13:52:27 +0100

    Created mod class

---
+++ src/main/java/com/enkigaming/minecraft/forge/enkilib/EnkiLib.java
@@ -0,0 +1,19 @@
+package com.enkigaming.minecraft.forge.enkilib;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = EnkiLib.MODID, name = EnkiLib.MODID, version = EnkiLib.VERSION, acceptableRemoteVersions = "*")
+public class EnkiLib
+{
+    public static final String NAME = "EnkiLib";
+    public static final String MODID = "EnkiLib";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        System.out.println("EnkiLib loaded!");
+    }
+}
